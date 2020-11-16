855b596d3ceeb5c12ca3653f05d44172d3563405
unknown <trevorgloe@gmail.com>
2020-11-16 00:39:27 -0800

    Fixed data storage bug

--- full_model_runs.m
+++ full_model_runs.m
@@ -1,11 +1,15 @@
 %run the model multiple times, changing alpha/beta
 
-num_viable_paths = zeros(2,10);
+num_viable_paths = zeros(2,5);
 %gamma = alpha/beta
-for gamma = [1e5:2e5:2e6]
+cnt = 1;
+for gamma = [1e5:2e5:1e6]
     beta = 0.00001;
     alpha = gamma*beta;
     
     flight_paths
-    num_viable_paths = length(optimal_plan_idx);
+    num = length(optimal_plan_idx);
+    num_viable_paths(1,cnt) = gamma;
+    num_viable_paths(2,cnt) = num;
+    cnt = cnt+1
 end
